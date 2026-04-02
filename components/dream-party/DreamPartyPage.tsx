162ff03acb1b12be63518fd6ab486043119308b3
sandeepsaini2822-code <sandeepsaini2822@gmail.com>
2026-04-03 01:22:25 +0530

    Update layout and founder section order

--- components/dream-party/DreamPartyPage.tsx
+++ components/dream-party/DreamPartyPage.tsx
@@ -125,7 +125,6 @@ export default function DreamPartyPage() {
 
       <HeroSection />
       <ServicesStrip />
-      <h1 style={{color: "red"}}>THIS IS NEW BUILD</h1>
 
       {loading ? (
         <div className="px-6 py-10 text-center">Loading menu...</div>
